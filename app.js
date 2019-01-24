b9e952f069d15763c08a67a7ade3e81306d0a66a
msanvarov <salim.anvarov@yahoo.com>
2019-01-23 22:21:35 -0500

    setup mvc module design pattern

---
+++ app.js
@@ -0,0 +1,17 @@
+$(function () {
+    var budgetController = (function () {
+
+    })();
+
+    var uiController = (function () {
+
+    })();
+
+// Global Controller
+    var controller = (function (budgetCtrl, uiCtrl) {
+
+    })(budgetController, uiController);
+
+
+});
+
