e656d0637e34c9fdaf0cd740729bf0827f97654c
msanvarov <salim.anvarov@yahoo.com>
2019-01-23 22:22:30 -0500

    added data properties

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 $(function () {
     var budgetController = (function () {
+        var data = {
+            allItems:
+                {
+                    exp: [],
+                    inc: []
+                },
+            totals: {
+                exp: 0,
+                inc: 0
+            },
+            budget: 0,
+            percentage: -1
+        };
 
     })();
 
     var uiController = (function () {
-
+        var DOMStrings = {
+            inputType: ".add__type",
+            inputDescription: ".add__description",
+            inputValue: ".add__value",
+            inputBtn: ".add__btn",
+            incomeContainer: ".income__list",
+            expensesContainer: ".expenses__list",
+            budgetIncome: ".budget__income--value",
+            budgetExpenses: ".budget__expenses--value",
+            budgetPercentage: ".budget__expenses--percentage",
+            budget: ".budget__value",
+            container: ".container"
+        };
     })();
 
 // Global Controller
